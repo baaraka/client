626f4ef9ebc2b6e3fe6ee82364baca7bcd08aa29
barakaganai <barakaganai01@gmail.com>
2023-03-26 20:26:24 +0300

    change variant

--- src/components/posts/post/Post.js
+++ src/components/posts/post/Post.js
@@ -50,7 +50,7 @@ export default function Post({ post, setCurrentId }) {
         {post.title}
       </Typography>
       <CardContent>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="body2" color="textSecondary" component="p">
           {post.message}
         </Typography>
       </CardContent>
