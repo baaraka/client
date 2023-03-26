b92b9706a21adee327ea680ab482037ffdeabc2f
barakaganai <barakaganai01@gmail.com>
2023-03-26 20:26:09 +0300

    prevent deprecated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
 import "./index.css";
